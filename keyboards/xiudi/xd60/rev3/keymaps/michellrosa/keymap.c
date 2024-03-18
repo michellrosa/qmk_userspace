198a77f209810cad84d0320465c74e24344557f5
Michell Eduardo do Amaral Rosa <michell@weg.net>
2024-03-18 11:50:56 -0300

    Add mouse movement keymap

--- keyboards/xiudi/xd60/rev3/keymaps/michellrosa/keymap.c
+++ keyboards/xiudi/xd60/rev3/keymaps/michellrosa/keymap.c
@@ -1,21 +1,139 @@
 #include QMK_KEYBOARD_H
 
+#include "digitizer.h"
+
+// Shift on Hold, Enter on Tap
+#define SHENT SFT_T(KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  // 0: Base Layer
-  LAYOUT_all(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_GRV,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,
-      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,
-      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_DEL,
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_RGUI, MO(1),    KC_LEFT, KC_DOWN,  KC_RIGHT),
-
-  // 1: Function Layer
-  LAYOUT_all(
-      QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_F13,   KC_F14,
-      KC_NO,   KC_WH_U, KC_UP,   KC_WH_D, KC_BSPC,KC_HOME,KC_CALC,KC_NO,  KC_INS, KC_NO,   KC_PSCR, KC_SCRL,  KC_PAUS,           KC_DEL,
-      KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL, KC_END, KC_PGDN,KC_NO,  KC_NO,  KC_NO,   KC_HOME, KC_PGUP,  KC_NO,             KC_ENT,
-      KC_LSFT, KC_NO,   KC_NO,   KC_APP,  BL_STEP,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_RSFT, KC_PGUP,  KC_INS,
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_RGUI, MO(1),    KC_HOME, KC_PGDN,  KC_END),
-
+    [0] = LAYOUT_60_ansi_split_bs_rshift_space(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,
+        CW_TOGG, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_ENT,
+        KC_LSFT, SHENT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_INS,
+        KC_LCTL, KC_LGUI, KC_LALT,          LT(1, KC_SPC),             LT(1, KC_SPC),    KC_NO,   KC_RALT, KC_APP,  KC_NO,   KC_RCTL, KC_RGUI
+    ),
+    [1] = LAYOUT_60_ansi_split_bs_rshift_space(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+        _______, _______, KC_UP,   _______, KC_PGUP, _______, _______, KC_HOME, KC_END,  _______, _______, _______, _______,          _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          _______,
+        _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   TG(2),
+        _______, _______, _______,          _______,                   _______,          KC_NO  , _______, KC_LEFT, KC_NO  , KC_DOWN, KC_RGHT
+    ),
+    [2] = LAYOUT_60_ansi_split_bs_rshift_space(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TG(2),
+        _______, _______, _______,          _______,                   _______,          KC_NO  , _______, _______, KC_NO  , _______, _______
+    ),
+#if 0
+    [x] = LAYOUT_60_ansi_split_bs_rshift_space(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,          _______,                   _______,          KC_NO  , _______, _______, KC_NO  , _______, _______
+    ),
+#endif
 };
+
+void keyboard_post_init_user(void) {
+    rgblight_disable_noeeprom();
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (layer_state_is(2)) {
+        float x = 1;
+        float y = 0;
+        switch (keycode) {
+            case KC_Q:
+            case KC_A:
+            case KC_Z:
+                x -= 0.1;
+            case KC_W:
+            case KC_S:
+            case KC_X:
+                x -= 0.1;
+            case KC_E:
+            case KC_D:
+            case KC_C:
+                x -= 0.1;
+            case KC_R:
+            case KC_F:
+            case KC_V:
+                x -= 0.1;
+            case KC_T:
+            case KC_G:
+            case KC_B:
+                x -= 0.1;
+            case KC_Y:
+            case KC_H:
+            case KC_N:
+                x -= 0.1;
+            case KC_U:
+            case KC_J:
+            case KC_M:
+                x -= 0.1;
+            case KC_I:
+            case KC_K:
+            case KC_COMM:
+                x -= 0.1;
+            case KC_O:
+            case KC_L:
+            case KC_DOT:
+                x -= 0.1;
+            case KC_P:
+            case KC_SCLN:
+            case KC_SLSH:
+                x -= 0.1;
+                break;
+            default:
+                return true; // Process all other keycodes normally
+        }
+        switch (keycode) {
+            case KC_Q:
+            case KC_W:
+            case KC_E:
+            case KC_R:
+            case KC_T:
+            case KC_Y:
+            case KC_U:
+            case KC_I:
+            case KC_O:
+            case KC_P:
+                y += 0.1;
+            case KC_A:
+            case KC_S:
+            case KC_D:
+            case KC_F:
+            case KC_G:
+            case KC_H:
+            case KC_J:
+            case KC_K:
+            case KC_L:
+            case KC_SCLN:
+                y += 0.1;
+            case KC_Z:
+            case KC_X:
+            case KC_C:
+            case KC_V:
+            case KC_B:
+            case KC_N:
+            case KC_M:
+            case KC_COMM:
+            case KC_DOT:
+            case KC_SLSH:
+                y += 0.1;
+                break;
+            default:
+                return true; // Process all other keycodes normally
+        }
+        if (record->event.pressed) { // Do something when pressed
+            digitizer_in_range_on();
+            digitizer_set_position(x, y);
+        }
+        return false; // Skip all further processing of this key
+    }
+    return true; // Process keycodes normally
+}
