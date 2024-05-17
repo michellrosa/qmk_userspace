6998da7db196fdb053fe42a1b9d4af341734a106
Michell Eduardo do Amaral Rosa <michell@weg.net>
2024-05-17 12:24:13 -0300

    BR Layout

--- keyboards/xiudi/xd60/rev3/keymaps/michellrosa/keymap.c
+++ keyboards/xiudi/xd60/rev3/keymaps/michellrosa/keymap.c
@@ -4,10 +4,7 @@
 #include "custom_mouse_keymap.h"
 #include "faker_layout.h"
 #include "mouse_manipulation.h"
-#include "keymap_brazilian_abnt2.h"
-
-// Shift on Hold, Enter on Tap
-#define FK_SENT SC_SENT // SFT_T(KC_ENT)
+// #include "keymap_brazilian_abnt2.h"
 
 enum keyboard_layers {
   _USL = 0, // Base Layer Ansii
@@ -18,15 +15,13 @@ enum keyboard_layers {
 
 #define _BL _USL
 
-#define FK_BR_QUOT _______ // TODO '"
-#define FK_BR_6    _______ // TODO 6¨
-#define FK_BR_ACUT _______ // TODO ´`
-#define FK_BR_LBRC KC_RBRC // [{
-#define FK_BR_RBRC KC_BSLS // ]}
-#define FK_BR_CCED _______ // TODO çÇ
-#define FK_BR_TILD _______ // TODO ~^
-#define FK_BR_BSLS KC_BSLS // \|
-#define FK_BR_SCLN KC_SLSH // ;:
+const custom_shift_key_t custom_shift_keys[] = {
+    {FKBR_6, FKBR_6_S},             // Shift 6 is ¨
+    {FKBR_ACUT, FKBR_ACUT_S},       // Shift ´ is `
+    //{FKBR_CCED, FKBR_CCED_S},     // Shift ç is Ç
+    {FKBR_TILD, FKBR_TILD_S},       // Shift ~ is ^
+};
+uint8_t NUM_CUSTOM_SHIFT_KEYS = sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if 0
@@ -34,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,
         CW_TOGG, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_ENT,
-        KC_LSFT, FK_SENT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_INS,
+        KC_LSFT, SC_SENT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_INS,
         KC_LCTL, KC_LGUI, KC_LALT,        LT(_FL, KC_SPC),           LT(_FL, KC_SPC),             KC_RALT, KC_APP,           KC_RCTL, KC_RGUI
     ),
 #else
@@ -42,15 +37,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,
         CW_TOGG, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_ENT,
-        KC_LSFT, FK_SENT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, KC_RSFT, KC_INS,
+        KC_LSFT, SC_SENT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, KC_RSFT, KC_INS,
         KC_LCTL, KC_LGUI, KC_LALT,        LT(_FL, KC_SPC),           LT(_FL, KC_SPC),    XXXXXXX, KC_RALT, KC_APP,  XXXXXXX, KC_RCTL, KC_RGUI
     ),
 #endif
     [_BRL] = LAYOUT_60_ansi_split_bs_rshift_space(
-        KC_ESC,  _______, _______, _______, _______, _______, BR_6,    _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, BR_ACUT, BR_LBRC,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, BR_CCED, BR_TILD, BR_RBRC,          _______,
-        _______, FK_SENT, _______, _______, _______, _______, _______, _______, _______, _______, _______, BR_SCLN, _______, _______, _______,
+        FKBR_QUOT, _______, _______, _______, _______, _______, FKBR_6, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, FKBR_ACUT, FKBR_LBRC,      _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, FKBR_CCED, FKBR_TILD, FKBR_RBRC,    _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, FKBR_SCLN, _______, _______, _______,
         _______, _______, _______,        _______,                   _______,            _______, _______, _______, _______, _______, _______
     ),
     [_FL] = LAYOUT_faker(
@@ -74,16 +69,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void keyboard_post_init_user(void) {
     rgblight_disable_noeeprom();
-    // Customise these values to desired behaviour
+#ifdef CONSOLE_ENABLE
     debug_enable=true;
     //debug_matrix=true;
     //debug_keyboard=true;
     //debug_mouse=true;
+#endif
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (layer_state_is(_ML)) { // Restrict processing for layer 2 active.
+    if (layer_state_is(_ML)) { // Restrict processing for layer _ML active.
         return process_record_user_mouse_manipulation(keycode, record);
     }
+    if (!process_custom_shift_keys(keycode, record)) { return false; }
+
+    switch (keycode) {
+    case FKBR_6:
+        if (record->event.pressed) {
+            register_code(KC_6);
+        } else {
+            unregister_code(KC_6);
+        }
+        // return false;
+        break;
+    }
+
     return true; // Process keycodes normally
 }
--- keyboards/xiudi/xd60/rev3/keymaps/michellrosa/rules.mk
+++ keyboards/xiudi/xd60/rev3/keymaps/michellrosa/rules.mk
@@ -5,6 +5,7 @@ MOUSEKEY_ENABLE = no
 BACKLIGHT_ENABLE = no
 
 SRC += mouse_manipulation.c
+SRC += features/custom_shift_keys.c
 
 CONSOLE_ENABLE = yes
 
